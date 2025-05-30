c8587e5d53efa4f783a375c3f89f9ff8f447b072
hoak04 <hugocarvalhof@gmail.com>
2025-05-30 02:23:20 -0300

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -51,6 +51,8 @@ dados = dados.reindex(columns=colunas_esperadas, fill_value=0)
 st.write("Colunas finais:", dados.columns.tolist())
 st.write("Shape final:", dados.shape)
 dados_normalizados = scaler.transform(dados.to_numpy())
+st.write("Features esperadas pelo modelo:", modelo.feature_names_in_)
+st.write("Features que estamos enviando:", dados.columns.tolist())
 
 
 # Predição
