866319557261b5d615f5986fce9933ca976fee5c
hoak04 <hugocarvalhof@gmail.com>
2025-05-31 08:23:34 -0300

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -63,11 +63,17 @@ df = pd.DataFrame([entrada])
 
 # Ordem correta das 24 colunas
 colunas_ordenadas = [
-    "Age","Sex","Ethnicity","BMI","Waist_Circumference","Fasting_Blood_Glucose","HbA1c"
-    ,"Blood_Pressure_Systolic","Blood_Pressure_Diastolic","Cholesterol_Total","Cholesterol_HDL"
-    ,"Cholesterol_LDL","GGT","Serum_Urate","Physical_Activity_Level","Dietary_Intake_Calories",
-    "Alcohol_Consumption","Smoking_Status","Family_History_of_Diabete"
+    "Age", "BMI", "Waist_Circumference", "Fasting_Blood_Glucose", 
+    "Blood_Pressure_Systolic", "Blood_Pressure_Diastolic",
+    "Cholesterol_Total", "Cholesterol_HDL", "Cholesterol_LDL", "GGT",
+    "Serum_Urate", "Dietary_Intake_Calories", "Family_History_of_Diabetes",
+    "Previous_Gestational_Diabetes", "Sex_Male", 
+    "Ethnicity_White", "Ethnicity_Black", "Ethnicity_Hispanic",
+    "Physical_Activity_Level_Low", "Physical_Activity_Level_Moderate",
+    "Alcohol_Consumption_None", "Alcohol_Consumption_Moderate",
+    "Smoking_Status_Former", "Smoking_Status_Never"
 ]
+
 
 
 # Reordenar e normalizar
